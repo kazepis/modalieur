ad4196b64c3de90725bd4816b0ad1b968b4ff8b4
Nikolas Kazepis <n.kazepis@tothekorr.com>
2023-09-21 21:19:43 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdlr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'modalieur';
+}
